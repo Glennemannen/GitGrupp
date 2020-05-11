0c030c022dcf04c3c05fcd4fbdfc1a4810a6a6bb
rmrthb <48605484+rmrthb@users.noreply.github.com>
2020-05-11 14:35:55 +0200

    Added a new class

---
+++ GitGrupp/ClassR.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitGrupp
+{
+    class ClassR
+    {
+        public void Message()
+        {
+            Console.WriteLine("This is Ray's branch!");
+        }
+    }
+}
