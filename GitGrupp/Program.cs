8db06787e7bfe22d366258716369ec8d1435b035
Glenn Jönsson <glennj83@hotmail.com>
2020-05-12 13:32:27 +0200

    ny

--- GitGrupp/Program.cs
+++ GitGrupp/Program.cs
@@ -7,6 +7,7 @@ namespace GitGrupp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Eric yo");
         }
     
     
